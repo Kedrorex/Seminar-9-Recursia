92ac39533e4e1f2d3652c5a9918195426896b59e
Kedrorex <Belousovvk@bk.ru>
2023-03-10 23:04:16 +0400

    inicilizacia

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+void RoadTon(int N)
+{
+  if(N==0)
+  {
+    return;
+  }
+  RoadTon(N-1);
+  Console.Write($"{N} ");
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadTon(number);
---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на
+//  вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int Sum(int number)
+// {
+//   int sum;
+//   if (number > 10)
+//   {
+//     Sum(number /= 10);
+    
+//   }
+// }
+
+// Решение Мухаммада
+int Sum(int value)
+{
+    if(value < 10)
+    {
+        return value % 10;
+    }
+    int temp = value % 10;
+    value = value/10;
+    return temp + Sum(value);
+}
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(Sum(number));
